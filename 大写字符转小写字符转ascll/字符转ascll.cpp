9ee8ea0a6f31abf121363fc71028c041701567e7
tokiyanagi <109673296+tokiyanagi@users.noreply.github.com>
2022-10-14 15:12:02 +0800

    Add files via upload

---
+++ 大写字符转小写字符转ascll/字符转ascll.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	char c1, c2;
+	c1 = getchar();
+	{if (c1 < 91)
+	c2 = c1 + 32;
+	else
+		c2 = c1;
+	}
+	printf("%c\n", c2);
+	printf("%d\n", c2);
+	return 0;
+}
